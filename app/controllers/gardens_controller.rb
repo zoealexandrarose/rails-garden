ef3c2fcfe7766d5f8929a31d3ea5dc11b5549a56
Zoe Rose zoealexandrarose@outlook.com <zoealexandrarose@outlook.com>
2022-11-16 21:36:57 +0100

    style

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,2 +1,9 @@
 class GardensController < ApplicationController
+  def index
+    @gardens = Garden.all
+  end
+
+  def show
+    @garden = Garden.find(params[:id])
+  end
 end
--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,2 +1,24 @@
 class PlantsController < ApplicationController
+  def new
+    @garden = Garden.find(params[:garden_id])
+    @plant = Plant.new
+  end
+
+  def create
+    @garden = Garden.find(params[:garden_id])
+    @plant = Plant.new(plant_params)
+    @plant.garden = @garden
+
+    if @plant.save
+      redirect_to garden_path(@garden)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def plant_params
+    params.require(:plant).permit(:name)
+  end
 end
